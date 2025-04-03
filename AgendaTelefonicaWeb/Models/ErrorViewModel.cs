67948d5796b9ce23a6cc5650c6d217f00b915261
Micael Ricardo <micael.ricardo@outlook.com>
2025-04-03 04:02:20 -0300

    Criacao projeto

---
+++ AgendaTelefonicaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgendaTelefonicaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

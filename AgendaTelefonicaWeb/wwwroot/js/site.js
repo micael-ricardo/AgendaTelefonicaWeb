21aa7ce0f06e88e10c06ed7c62b77a0c714b30cd
Micael Ricardo <micael.ricardo@outlook.com>
2025-04-04 09:56:54 -0300

    Inserir datatable com filtro + mascara

--- AgendaTelefonicaWeb/wwwroot/js/site.js
+++ AgendaTelefonicaWeb/wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function aplicarMascaraTelefone() {
+    const telefoneInput = document.querySelectorAll('.telefone');
 
-// Write your JavaScript code.
+    if (telefoneInput) {
+        Inputmask({
+            mask: ['(99) 9999-9999', '(99) 99999-9999'],
+            keepStatic: true,
+            removeMaskOnSubmit: true
+        }).mask(telefoneInput);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', aplicarMascaraTelefone);
+
+
+$(document).ready(function () {
+    $('#tabelaContatos').DataTable({
+        "paging": true, 
+        "pageLength": 5, 
+        "lengthChange": true, 
+        "info": true, 
+        "autoWidth": false, 
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese-Brasil.json"
+        }
+    });
+});
